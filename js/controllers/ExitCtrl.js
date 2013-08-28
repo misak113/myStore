c722bc8d2c6f9f713cf1b056e5e01db8142b8a0b
Michael Zabka <zabka.michael@seznam.cz>
2013-08-28 21:05:55 +0200

    no last page exit

--- js/controllers/ExitCtrl.js
+++ js/controllers/ExitCtrl.js
@@ -1,9 +1,11 @@
 
 
-function ExitCtrl($scope, native) {
+function ExitCtrl($scope, native, offlineStorage) {
 	if (!native.ready())
 		return;
 
+	offlineStorage.set('app-lastVisitedPageRoute', null);
+	
 	// @todo this is depricated
 	native.WL.App.close();
 
