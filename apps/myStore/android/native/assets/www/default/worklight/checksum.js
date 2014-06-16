6e967f2987a11ac3cd33df03b501d7e5e0e148f2
Michael Zabka <zabka.michael@seznam.cz>
2014-06-16 22:30:38 +0200

    build wl

--- apps/myStore/android/native/assets/www/default/worklight/checksum.js
+++ apps/myStore/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":1486142174,"date":1402874461579,"machine":"Michaela"};
-/* Date: Mon Jun 16 01:21:01 CEST 2014 */
+var WL_CHECKSUM = {"checksum":1486142174,"date":1402950441923,"machine":"Michaela"};
+/* Date: Mon Jun 16 22:27:21 CEST 2014 */
